881bece6c3a5e15ff56223bfaea38c53b9637350
maxwell.maier <maxwellmaier89@gmail.com>
2024-04-04 19:59:26 -0500

    exercises completed

--- control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/ArrayPractice.java
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/ArrayPractice.java
@@ -1,2 +1,65 @@
-package org.example;public class ArrayPractice {
-}
+package org.launchcode;
+import java.util.ArrayList;
+//import java.util.Collections;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.example.Main.sumEven;
+
+public class ArrayPractice {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
+        String phrase = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+        System.out.println("All values:");
+        for (int i : integerArray) {
+            System.out.println(i);
+        }
+        String[] words = phrase.split(" ");
+        System.out.println(Arrays.toString(words));
+        String[] sentences = phrase.split("\\.");
+        System.out.println(Arrays.toString(sentences));
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+
+        int sumOfEvenNumbers = sumEven(numbers);
+        System.out.println("Sum of even numbers: " + sumOfEvenNumbers);
+
+        String[] fruitwords = {"apple", "banana", "orange", "grape", "kiwi", "melon", "peach"};
+
+        System.out.println("Words with exactly 5 letters:");
+        printWordsWithFiveLetters(words);
+    }
+
+    public static void printWordsWithFiveLetters(String[] words) {
+        for (String word : words) {
+            if (word.length() == 5) {
+                System.out.println(word);
+            }
+        }
+    }
+
+        public static int sumEven (ArrayList < Integer > arr) {
+            int total = 0;
+            for (int integer : arr) {
+                if (integer % 2 == 0) {
+                    total += integer;
+                }
+            }
+            return total;
+        }
+    }
+
+
+
---
+++ control-flow-and-collections/exercises/collections-exercises/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0) {
+                total += integer;
+            }
+        }
+        return total;
+    }
+}
